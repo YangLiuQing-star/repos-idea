57b7178199167119ec747261474ebd871d8530b6
yangliuqing <yangliuqing@tgcat.cn>
2020-07-12 10:39:00 +0800

    本地工程git-test添加到git

---
+++ git-test/src/com/example/GitTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * @author YangLiuQing
+ * @date 2020/7/12 10:28
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project...");
+        System.out.println("hello git project...");
+        System.out.println("hello git project...");
+        System.out.println("hello git project...");
+        System.out.println("hello git project...");
+    }
+
+    public void sayHello(){
+        System.out.println("尚硅谷，你好...");
+    }
+}
